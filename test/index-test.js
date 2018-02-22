b41fb38398658a38a60ee82d9aea7bc817bff029
zmahfuz817 <zunayed.lm@gmail.com>
2018-02-22 03:16:03 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -10,7 +10,7 @@ describe('what-is-a-test', () => {
 
   describe("Height", () => {
     it("is less than 74", () => {
-      expect(40).toBeLessThan(74)
+      expect("40").toBeLessThan("74")
     })
   })
 
